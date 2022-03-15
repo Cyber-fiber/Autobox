4ecf2293c3d4a0953e0e4839fbf5d9de951470b5
Cyber-fiber <85816800+Cyber-fiber@users.noreply.github.com>
2022-03-15 20:58:14 +0530

    Add files via upload

---
+++ scripts/2-user.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+echo -ne "
+-------------------------------------------------------------------------
+       █████╗ ██╗   ██╗████████╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗
+      ██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔══██╗██╔═══██╗╚██╗██╔╝
+      ███████║██║   ██║   ██║   ██║   ██║██████╔╝██║   ██║ ╚███╔╝ 
+      ██╔══██║██║   ██║   ██║   ██║   ██║██╔══██╗██║   ██║ ██╔██╗ 
+      ██║  ██║╚██████╔╝   ██║   ╚██████╔╝██████╔╝╚██████╔╝██╔╝ ██╗
+      ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝        
+-------------------------------------------------------------------------
+                    Automatic Arch Linux Installer
+                        SCRIPTHOME: Autobox
+-------------------------------------------------------------------------
+
+Installing AUR Softwares
+"
+
+if [[ ! $AUR_HELPER == none ]]; then
+  cd ~
+  git clone "https://aur.archlinux.org/$AUR_HELPER.git"
+  cd ~/$AUR_HELPER
+  makepkg -si --noconfirm
+  # sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
+  # stop the script and move on, not installing any more packages below that line
+  sed -n '/'$INSTALL_TYPE'/q;p' ~/Autobox/pkg-files/aur-pkgs.txt | while read line
+  do
+    if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
+      # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+      continue
+    fi
+    echo "INSTALLING: ${line}"
+    $AUR_HELPER -S --noconfirm --needed ${line}
+  done
+fi
+
+export PATH=$PATH:~/.local/bin
+
+# Theming DE if user chose FULL installation
+if [[ $INSTALL_TYPE == "FULL" ]]; then
+  if [[ $DESKTOP_ENV == "openbox" ]]; then
+    cd ~
+    git clone https://github.com/stojshic/dotfiles-openbox
+    ./dotfiles-openbox/install-titus.sh
+  fi
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    SYSTEM READY FOR 3-post-setup.sh
+-------------------------------------------------------------------------
+"
+exit
